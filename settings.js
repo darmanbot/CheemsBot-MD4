94895ee1d3483c6f2175b8a335673eb654f01de2
darmanbot <140476994+darmanbot@users.noreply.github.com>
2023-07-25 11:42:48 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,7 +54,7 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
+global.vcardowner = ['6281265488927'] //ur owner number
 global.ownername = "Darman" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
